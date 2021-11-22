94e48776e563eb6b64734786dd24904ed9216f54
Stephanie Quon <42529732+StephanieQuon@users.noreply.github.com>
2021-11-22 02:23:22 -0800

    Update stabilze.m

--- stabilze.m
+++ stabilze.m
@@ -1,7 +1,12 @@
 %% TAKEN FROM: www.mathworks.com/help/vision/ug/video-stabilization-using-point-feature-matching.html
+%% References
+%% [1] Tordoff, B; Murray, DW. "Guided sampling and consensus for motion estimation." European Conference n Computer Vision, 2002.
+%% [2] Lee, KY; Chuang, YY; Chen, BY; Ouhyoung, M. "Video Stabilization using Robust Feature Trajectories." National Taiwan University, 2009.
+%% [3] Litvin, A; Konrad, J; Karl, WC. "Probabilistic video stabilization using Kalman filtering and mosaicking." IS&T/SPIE Symposium on Electronic Imaging, Image and Video Communications and Proc., 2003.
+%% [4] Matsushita, Y; Ofek, E; Tang, X; Shum, HY. "Full-frame Video Stabilization." Microsoft® Research Asia. CVPR 2005.
 
 %% ------------------------------- Setup ----------------------------------
-vidStable('id_CSS027_1 copy.mp4', 'CSS027_1_stb');
+vidStable('input_video.mp4', 'input_video_stb.mp4');
 
 %% --------------------------- Stabilize Video ----------------------------
 
